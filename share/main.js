a5d3ffd3386b4822d1e182c12bb5790cd9b4bca1
weizhansheng <weizs@mingyuanyun.com>
2019-02-18 20:46:24 +0800

    update version

--- share/main.js
+++ share/main.js
@@ -15,22 +15,33 @@ function runQueue(queue, fn, end) {
     }
 }
 
+function getUrl(uid) {
+    return ['http://www.shandianji.com.cn/index/share/share_download?uid=', uid, '&type=3'].join('')
+}
+
 
 var uids = [430040, 626545]
 
-var iframe = document.getElementById('iframe')
+// var iframe = document.getElementById('iframe')
 var message = document.getElementById('message')
+var win = null
 runQueue(uids, function (uid, next) {
-    message.innerText = '开始分享：' + iframe.getAttribute('src') + '\n' + uid
-    iframe.onload = function () {
-        message.innerText = iframe.getAttribute('src') + '\n执行结束，准备分享：' + uid
-        setTimeout(next, 3000)
-    };
-    iframe.setAttribute('src', '')
-    iframe.setAttribute(
-        'src',
-        ['http://www.shandianji.com.cn/index/share/share_download?uid=', uid, '&type=3'].join('')
-    )
+    var url = getUrl(uid)
+    message.innerText = '开始分享：' + url
+    // iframe.onload = function () {
+    //     message.innerText = iframe.getAttribute('src') + '\n执行结束，准备分享：' + uid
+    //     setTimeout(next, 3000)
+    // };
+    // iframe.setAttribute('src', '')
+    // iframe.setAttribute(
+    //     'src',
+    //     ['http://www.shandianji.com.cn/index/share/share_download?uid=', uid, '&type=3'].join('')
+    // )
+    win = window.open(url);
+    setTimeout(function () {
+        win && win.close()
+        next()
+    }, 1000)
 }, function () {
     message.innerText = '执行完毕'
 })
