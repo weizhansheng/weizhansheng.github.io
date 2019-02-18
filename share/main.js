3631a469deae59e062849a0259632bf1f4e3e634
weizhansheng <weizs@mingyuanyun.com>
2019-02-18 20:01:38 +0800

    open new window

--- share/main.js
+++ share/main.js
@@ -17,18 +17,24 @@ function runQueue(queue, fn, end) {
 
 
 var uids = [430040, 430041, 430042, 430043, 430044, 430045]
-var iframe = document.getElementById('iframe')
+
+// var iframe = document.getElementById('iframe')
 var message = document.getElementById('message')
 runQueue(uids, function (uid, next) {
-    message.innerText = '开始分享：' + uid
-    iframe.onload = function () {
-        message.innerText = '执行结束，准备分享：' + uid
-        setTimeout(next, 2000)
-    };
-    iframe.setAttribute(
-        'src',
-        ['http://www.shandianji.com.cn/index/share/share_download?uid=', uid, '&type=3'].join('')
-    )
+    // message.innerText = '开始分享：' + uid
+    // iframe.onload = function () {
+    //     message.innerText = '执行结束，准备分享：' + uid
+    //     setTimeout(next, 2000)
+    // };
+    // iframe.setAttribute(
+    //     'src',
+    //     ['http://www.shandianji.com.cn/index/share/share_download?uid=', uid, '&type=3'].join('')
+    // )
+    var win = window.open(['http://www.shandianji.com.cn/index/share/share_download?uid=', uid, '&type=3'].join(''))
+    setTimeout(function () {
+        win.close()
+        next()
+    }, 3000)
 }, function () {
     message.innerText = '执行完毕'
 })
