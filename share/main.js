f8cf8c1e52b4963867658c25e6057925ecf52125
weizhansheng <weizs@mingyuanyun.com>
2019-02-18 20:12:49 +0800

    update version

--- share/main.js
+++ share/main.js
@@ -21,18 +21,15 @@ var uids = [430040, 430041, 430042, 430043, 430044, 430045]
 var iframe = document.getElementById('iframe')
 var message = document.getElementById('message')
 runQueue(uids, function (uid, next) {
-    message.innerText = '开始分享：' + uid
+    message.innerText = '开始分享：' + iframe.getAttribute('src') + '\n' + uid
     iframe.onload = function () {
-        message.innerText = '执行结束，准备分享：' + uid
-        setTimeout(next, 2000)
+        message.innerText = iframe.getAttribute('src') + '\n执行结束，准备分享：' + uid
+        setTimeout(next, 3000)
     };
     iframe.setAttribute(
         'src',
         ['http://www.shandianji.com.cn/index/share/share_download?uid=', uid, '&type=3'].join('')
     )
-    setTimeout(function () {
-        next()
-    }, 1000)
 }, function () {
     message.innerText = '执行完毕'
 })
